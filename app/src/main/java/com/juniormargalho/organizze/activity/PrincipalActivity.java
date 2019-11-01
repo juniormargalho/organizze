a12164fc999178e01df2cfd546c4c48268049fba
juniormargalho <juniormargalho@gmail.com>
2019-11-01 00:06:52 -0300

    interfacePrincipal

--- app/src/main/java/com/juniormargalho/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/juniormargalho/organizze/activity/PrincipalActivity.java
@@ -10,10 +10,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
 
 import com.juniormargalho.organizze.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    private MaterialCalendarView calendarView;
+    private TextView textoSaudacao, textoSaldo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,12 @@ public class PrincipalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        textoSaudacao = findViewById(R.id.textSaudacao);
+        textoSaldo = findViewById(R.id.textSaldo);
+        calendarView = findViewById(R.id.calendarView);
+
+        configuraCalendarView();
 
         /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +51,20 @@ public class PrincipalActivity extends AppCompatActivity {
 
     public void adicionarReceita(View view){
         startActivity(new Intent(this, ReceitasActivity.class));
+    }
+
+    public void configuraCalendarView(){
+        CharSequence meses[] =
+                {"Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+        calendarView.setTitleMonths(meses);
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+            }
+        });
+
     }
 
 }
