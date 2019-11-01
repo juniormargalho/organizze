2f010be41dbdde8a680bf2bedd976f847019c199
juniormargalho <juniormargalho@gmail.com>
2019-11-01 00:52:33 -0300

    toolbarSignOut

--- app/src/main/java/com/juniormargalho/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/juniormargalho/organizze/activity/PrincipalActivity.java
@@ -9,10 +9,14 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.juniormargalho.organizze.R;
+import com.juniormargalho.organizze.config.ConfiguracaoFirebase;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
@@ -21,12 +25,14 @@ public class PrincipalActivity extends AppCompatActivity {
 
     private MaterialCalendarView calendarView;
     private TextView textoSaudacao, textoSaldo;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Organizze");
         setSupportActionBar(toolbar);
 
         textoSaudacao = findViewById(R.id.textSaudacao);
@@ -45,12 +51,36 @@ public class PrincipalActivity extends AppCompatActivity {
         });*/
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuSair :
+                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+                autenticacao.signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
     public void adicionarDespesa(View view){
         startActivity(new Intent(this, DespesasActivity.class));
+
     }
 
     public void adicionarReceita(View view){
         startActivity(new Intent(this, ReceitasActivity.class));
+
     }
 
     public void configuraCalendarView(){
